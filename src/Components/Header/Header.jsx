c564b30ac297af6bb498c9f6783ad43cd4fe3a81
Shane Michel <info@shanemichel.net>
2023-07-05 17:21:03 -0400

    Added folders and icons
    installed react icons

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+import {AiOutlineShoppingCart} from 'react-icons/ai'
+
+
+function Header() {
+  return (
+    <div className='header-container'>
+        
+        <AiOutlineShoppingCart />
+    </div>
+  )
+}
+
+export default Header
